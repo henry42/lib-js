e1f842de628003e84ab399da0982c4064a8bea45
unknown <Henry@.(none)>
2009-12-24 18:03:05 +0800

    selector update

--- njs/njs.selector.js
+++ njs/njs.selector.js
@@ -20,7 +20,7 @@
          E:nth-of-type(n)
          E:nth-last-of-type(n)
          */
-        "pseudo$child": new RegExp("^:(nth-(?:last-)?(?:child|of-type))\\([^\\)]\\)"),
+        "pseudo$child": new RegExp("^:(nth-(?:last-)?(?:child|of-type))\\(\\s*(([-+]?)(\\d*)(n)(?:\\s*([-+])\\s*(\\d+))?|([-+]?)(\\d+)|odd|even)\\s*\\)"),
 		/*
 		 E:not()
 		*/
@@ -50,7 +50,7 @@
          E:target
          */
         "pseudo$normal": null,
-		"element": new RegExp("^(?:\\s+|\\s*(>)\\s*)(" + chars + "+)?"),
+		"element": new RegExp("^(?:\\s*(>)\\s*|\\s+)(" + chars + "+)?")
     }
     var parse_f = 
     {
@@ -109,10 +109,10 @@
         },
         "attr": function(m, elems)
         {
-            var n = m[2], o = m[3], v = m[4], f = o ? filter.attr[o] : filter.attr["="];
+            var n = m[2], o = m[3], v = m[4], f = o ? filter.attr[o] : filter.attr["exist"];
             var r = [];
             if (!f) 
-                throw "unknown operation : " + o;
+                throw "unknown operation " + o;
             for (var i = 0, len = elems.length; i < len; i++) 
             {
                 if (f(elems[i], n, v)) 
@@ -134,110 +134,175 @@
         },
         "pseudo$child": function(m, elems)
         {
-            return elems;
+			
+			var filter = filter["pseudo$child"][m[1]];
+			if(!filter)
+				throw "unknown pseudo " + m[1];
+
+			var r = [];
+            for (var i = 0, len = elems.length; i < len; i++) 
+            {
+                if (filter(elems[i] , m)) 
+                    r.push(elems[i]);
+            }
+            elems.length = 0;
+            return $njs.array.copy($njs.array.unique(r), elems);
         },
         "pseudo$normal": function(m, elems)
         {
             return elems;
         }
     }
+
+	var leftCount = function(elem , type)
+	{
+		var i = 0;
+		while(elem = elem.previousSibling)
+			if(elem.nodeType === 1 && (!type || $njs.element.nodeName(elem , type)))
+				i++;
+		return i;
+	}
+	var rightCount = function(elem , type)
+	{
+		var i = 0;
+		while(elem = elem.nextSibling)
+			if(elem.nodeType === 1 && (!type || $njs.element.nodeName(elem , type)))
+				i++;
+		return i;
+	}
+
     var filter = 
     {
         "attr": 
         {
+			"exist" : function(elem , n ,v)
+			{
+				return elem.getAttribute(n) != null;
+			},
             "=": function(elem, n, v)
             {
-                return v ? elem.getAttribute(n) == v : elem.hasAttribute(n);
-            }
+                return elem.getAttribute(n) == v;
+            },
+			"~=" : function(elem , n , v)
+			{
+				return $njs.array.inArray(v , (elem.getAttribute(n) || "").split(/\s+/)) != -1;
+			},
+			"|=" : function(elem , n , v)
+			{
+				return (elem.getAttribute(n) || "").indexOf(v + "-") == 0;
+			},
+			"^=" : function(elem , n , v)
+			{
+				return v ? (elem.getAttribute(n) || "").indexOf(v) == 0 : 0;
+			},
+			"$=" : function(elem , n , v)
+			{
+				return v ? (elem.getAttribute(n) || "").slice(-v.length) == v : 0;
+			},
+			"*=" : function(elem , n , v)
+			{
+				return v ? (elem.getAttribute(n) || "").indexOf(v) >= 0 : 0;
+			}
         },
         "pseudo$normal": 
         {
             "root": function(elem)
             {
-                return true;
+                return !elem.parentNode && $njs.element.nodeName(elem , "html");
             },
             "first-child": function(elem)
             {
-                return true;
+                while(elem = elem.previousSibling)
+					if(elem.nodeType === 1)
+						return false;
+				return true;
             },
             "last-child": function(elem)
             {
-                return true;
+				while(elem = elem.nextSibling)
+					if(elem.nodeType === 1)
+						return false;
+				return true;
             },
             "first-of-type": function(elem)
             {
-                return true;
+				var type = elem.nodeName;
+                while(elem = elem.previousSibling)
+					if(elem.nodeType === 1 && $njs.element.nodeName(elem , type))
+						return false;
+				return true;
             },
             "last-of-type": function(elem)
             {
-                return true;
+                var type = elem.nodeName;
+                while(elem = elem.nextSibling)
+					if(elem.nodeType === 1 && $njs.element.nodeName(elem , type))
+						return false;
+				return true;
             },
             "only-child": function(elem)
             {
-                return true;
+                return this["first-child"](elem) && this["last-child"](elem);
             },
             "only-of-type": function(elem)
             {
-                return true;
+                return this["first-of-type"](elem) && this["last-of-type"](elem);
             },
             "empty": function(elem)
             {
-                return true;
-            },
-            "link": function(elem)
-            {
-                return true;
-            },
-            "visited": function(elem)
-            {
-                return true;
-            },
-            "active": function(elem)
-            {
-                return true;
-            },
-            "hover": function(elem)
-            {
-                return true;
-            },
-            "focus": function(elem)
-            {
-                return true;
-            },
-            "target": function(elem)
-            {
-                return true;
+                return !elem.firstChild;
             },
             "enabled": function(elem)
             {
-                return true;
+                return elem.disabled === false && elem.type !== "hidden";
             },
             "disabled": function(elem)
             {
-                return true;
+                return elem.disabled === true;
             },
             "checked": function(elem)
             {
-                return true;
+                return elem.checked === true;
             }
         },
         "pseudo$child": 
         {
-            "nth-child": function(elem)
+			"nth" : function(elem , m , func , type)
+			{
+				var ind = func(elem , type);
+				var n1 , n2;
+                if(m[2] == "odd")
+					n1 = 2 , n2 = 1; 
+				else if(m[2] == "even")
+					n1 = 2 , n2 = 0;
+				else if(m[5] == "n")
+					n1 = 1 * (m[3] + m[4]) , n2 = 1 * ((m[6] + m[7]) || 0);
+				if(n1)
+				{
+					var a = ind - n2;
+					return a / n1 > 0 && a % n1;
+				}
+				else
+				{
+					var _ind = 1 * (m[8] + m[9]);
+					return ind == _ind;
+				}
+			},
+            "nth-child": function(elem , m)
             {
-                return true;
+				return this["nth"](elem , m , leftCount);
             },
-            "nth-last-child": function(elem)
+            "nth-last-child": function(elem , m)
             {
-                return true;
+                return this["nth"](elem , m , rightCount);
             },
-            "nth-of-type": function(elem)
+            "nth-of-type": function(elem , m)
             {
-                return true;
+                return this["nth"](elem , m , leftCount , elem.nodeName);
             },
-            "nth-last-of-type": function(elem)
+            "nth-last-of-type": function(elem , m)
             {
-                return true;
+                return this["nth"](elem , m , rightCount, elem.nodeName);
             }
         }
     };
@@ -269,9 +334,9 @@
 				for (var n in parse) 
 					if (arr = parse[n].exec(a)) 
 					{
-						console.info(arr);
+						//console.info(arr);
 						a = a.substr(arr[0].length);
-						LOG("op:","\t",n , "\t\t\t\t\t\t" + arr.slice().join("|"));
+						LOG("op:","\t",n , "\t\t\t\t\t\t" + arr.slice().join("$"));
 						if(n != "not")
 							this.cache.push({"op" : n , "da" : arr});
 						else
@@ -280,7 +345,7 @@
 			}
 			while (last != a)
 			if($njs.trim(a).length > 0)
-				throw "unknown path :" + a;
+				throw "unknown path " + a;
 			 
 		}
         this.find = function(root)
@@ -298,7 +363,7 @@
 					parse_f[this.cache[i]["op"]](this.cache[i]["da"], root);
 				else
 					for(var j = 0 ; j < root.length ; j++)
-						if(this.cache[i]["selector"].find(root[j]).length == 0)
+						if(this.cache[i]["op"] == "not" && this.cache[i]["selector"].find(root[j]).length != 0)
 							root.splice(j--,1);
 			}
             return root;
