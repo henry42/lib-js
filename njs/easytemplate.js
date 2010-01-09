dc433c8e56b6c1cd98ada5fb3f8d11378f239b97
henry <henrypares@163.com>
2010-01-09 08:31:44 +0800

    temp

--- njs/easytemplate.js
+++ njs/easytemplate.js
@@ -10,22 +10,32 @@
 {
 	var wrapText = function(t)
 	{
-		t = t.replace(/\\/g, '\\\\');
-		t = t.replace(/\n/g, '\\n');
-		t = t.replace(/"/g, '\\"');
+		t = t.replace(/\\/g, "\\\\").replace(/\n/g, "\\n").replace(/"/g, "\\\"");
 		return "_Write(\"" + t + "\");";
 	}
 	var wrapFunction = function(t)
 	{
-		return t.charAt(0) == "="? "_Write(" + t.substr(1) + ");" : t;
+		return t.charAt(0) == "="? "_Write(" + t.substr(1) + ");" : t + ";";
 	}
 	var _newT = function(func)
 	{
-		this.process = function(data)
+		this.process = function(data,onerror)
 		{
 			var arr = [];
-			var _Write = function(t){arr.push(t);};
-			func(_Write , data);
+			try
+			{
+				func(function(t){arr.push(t);} , data);
+			}
+			catch (e)
+			{
+				if(typeof onerror == "function")
+				{
+					onerror(arr.join("") + "[" + e.message + "]");
+					return "";
+				}
+				else
+					throw e;
+			}
 			return arr.join("");
 		}
 	}
@@ -34,8 +44,7 @@
 		var l = 0 , n , nn , r = ["function(_Write,_CONTEXT){with(_CONTEXT){"] , beginTag = beginTag || "<#" , endTag = endTag || "#>";
 		while((n = text.indexOf(beginTag,l)) != -1 && (nn = text.indexOf(endTag,n)) != -1)
 		{
-			r.push(wrapText(text.substring(l,n)));
-			r.push(wrapFunction(text.substring(n + 2,nn)));
+			r.push(wrapText(text.substring(l,n)),wrapFunction(text.substring(n + 2,nn)));
 			l = nn + 2;
 		}
 		if(l < text.length)
